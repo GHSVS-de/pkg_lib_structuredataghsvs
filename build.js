05bf1d6dffec2bc62f2bc93cee870307b64533aa
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2021-10-16 12:13:15 +0200

    Review build JavaScript and things.

--- build.js
+++ build.js
@@ -4,168 +4,154 @@ const rimRaf = util.promisify(require("rimraf"));
 const chalk = require('chalk');
 // const exec = util.promisify(require('child_process').exec);
 const path = require('path');
+const replaceXml = require('./build/replaceXml.js');
+const helper = require('./build/helper.js');
+
+let thisPackages = [];
 
 const {
-	author,
-	creationDate,
-	copyright,
 	filename,
 	name,
-	nameReal,
 	version,
-	licenseLong,
-	minimumPhp,
-	maximumPhp,
-	minimumJoomla,
-	maximumJoomla,
-	allowDowngrades,
-	description,
 } = require("./package.json");
 
-const vendorPackageName = 'spatie/schema-org';
+const vendorPath = `./_composer/vendor`;
 const packagesDir = `./package/packages`;
 const libDir = `${packagesDir}/lib_structuredataghsvs`;
-
 const pkgManifest = `./package/pkg_${filename}.xml`;
-const libManifest = `${path.join(libDir, filename)}.xml`;
-
-//process.exit(0);
-
-async function cleanOut (cleanOuts) {
-	for (const file of cleanOuts)
-	{
-		await rimRaf(file).then(
-			answer => console.log(chalk.redBright(`rimrafed: ${file}.`))
-		).catch(error => console.error('Error ' + error));
-	}
-}
+const manifestFileName = `pkg_${filename}.xml`;
+const Manifest = `${__dirname}/package/${manifestFileName}`;
 
 (async function exec()
 {
-	// ### House cleaning - START
 	let cleanOuts = [
 		`./package`,
 		`./dist`,
 	];
 
-	await cleanOut(cleanOuts);
-	// ### House cleaning - END
+	await helper.cleanOut(cleanOuts);
 
-	// ### Get Spatie version - START
-	let sourceInfos = '';
-	const {
-		packages,
-	} = require("./_composer/vendor/composer/installed.json");
-
-	packages.forEach((package) => {
-		if (package.name === vendorPackageName)
-		{
-			sourceInfos = package.version;
-		}
-	});
-
-	console.log(chalk.yellowBright(`Using ${vendorPackageName} version ${sourceInfos}`));
-	// ### Get Spatie version - END
+	versionSub = await helper.findVersionSub (
+		path.join(__dirname, vendorPath, `composer/installed.json`),
+			'spatie/schema-org');
+	console.log(chalk.magentaBright(`versionSub identified as: "${versionSub}"`));
 
 	await console.log(chalk.redBright(`Be patient! Copy actions!`));
 
-	// Copy /src/ base folder.
-	await fse.copy("./src", "./package"
+	await fse.copy(`./src`, `./package`
 	).then(
-		answer => console.log(chalk.yellowBright(`Copied ./src to ./package.`))
+		answer => console.log(chalk.yellowBright(`Copied "./src" to "./package".`))
 	);
 
 	// Copy vendor to lib_folder.
-	await console.log(chalk.redBright(`Be very patient! Composer copy actions!`));
-	await fse.copy("./_composer/vendor", `${libDir}/vendor`
+	await console.log(chalk.redBright(`Be patient! Composer copy actions!`));
+	await fse.copy(`${vendorPath}`, `${libDir}/vendor`
 	).then(
-		answer => console.log(chalk.yellowBright(`Copied _composer/vendor to ${libDir}/vendor.`))
+		answer => console.log(chalk.yellowBright(
+			`Copied "_composer/vendor" to "${libDir}/vendor".`))
 	);
 
-	let xml = await fse.readFile(libManifest, { encoding: "utf8" });
-	xml = xml.replace(/{{name}}/g, name);
-	xml = xml.replace(/{{nameUpper}}/g, name.toUpperCase());
-	xml = xml.replace(/{{authorName}}/g, author.name);
-	xml = xml.replace(/{{creationDate}}/g, creationDate);
-	xml = xml.replace(/{{copyright}}/g, copyright);
-	xml = xml.replace(/{{licenseLong}}/g, licenseLong);
-	xml = xml.replace(/{{authorUrl}}/g, author.url);
-	xml = xml.replace(/{{version}}/g, version);
-	xml = xml.replace(/{{minimumPhp}}/g, minimumPhp);
-	xml = xml.replace(/{{maximumPhp}}/g, maximumPhp);
-	xml = xml.replace(/{{minimumJoomla}}/g, minimumJoomla);
-	xml = xml.replace(/{{maximumJoomla}}/g, maximumJoomla);
-	xml = xml.replace(/{{allowDowngrades}}/g, allowDowngrades);
-	xml = xml.replace(/{{filename}}/g, filename);
-	xml = xml.replace(/{{description}}/g, description);
-
-	await fse.writeFile(libManifest, xml, { encoding: "utf8" }
-	).then(
-		answer => console.log(chalk.yellowBright(`Replaced entries in ${libManifest}.`))
-	);
-// process.exit(0);
-
-	// ### Pack the library (child) - START.
-	let zip = new (require("adm-zip"))();
-	const libZipFilename = `${name}-${version}_${sourceInfos}.zip`;
-
-	let zipFilename = `${packagesDir}/${libZipFilename}`;
-	zip.addLocalFolder(libDir, false);
-	zip.writeZip(`${zipFilename}`);
-	console.log(chalk.greenBright(`${packagesDir}/${libZipFilename} written.`));
-
-	cleanOuts = [
-		`${libDir}`,
-	];
-	await cleanOut(cleanOuts).then(
-		answer => console.log(chalk.yellowBright(`Child done. Start with package now.`))
-	);
-	// ### Pack the library (child) - END.
-
-	xml = await fse.readFile(pkgManifest, { encoding: "utf8" });
-	xml = xml.replace(/{{zipFilenameLibrary}}/g, libZipFilename);
-	xml = xml.replace(/{{name}}/g, name);
-	xml = xml.replace(/{{nameUpper}}/g, name.toUpperCase());
-	xml = xml.replace(/{{authorName}}/g, author.name);
-	xml = xml.replace(/{{creationDate}}/g, creationDate);
-	xml = xml.replace(/{{copyright}}/g, copyright);
-	xml = xml.replace(/{{licenseLong}}/g, licenseLong);
-	xml = xml.replace(/{{authorUrl}}/g, author.url);
-	xml = xml.replace(/{{version}}/g, version);
-	xml = xml.replace(/{{minimumPhp}}/g, minimumPhp);
-	xml = xml.replace(/{{maximumPhp}}/g, maximumPhp);
-	xml = xml.replace(/{{minimumJoomla}}/g, minimumJoomla);
-	xml = xml.replace(/{{maximumJoomla}}/g, maximumJoomla);
-	xml = xml.replace(/{{allowDowngrades}}/g, allowDowngrades);
-	xml = xml.replace(/{{filename}}/g, filename);
-	xml = xml.replace(/{{description}}/g, description);
-
-	await fse.writeFile(pkgManifest, xml, { encoding: "utf8" }
-	).then(
-		answer => console.log(chalk.yellowBright(`Replaced entries in ${pkgManifest}.`))
-	);
-
-	// Create new dist dir.
-	if (!(await fse.exists("./dist")))
+	if (!(await fse.exists(`./dist`)))
 	{
-    	await fse.mkdir("./dist"
+    	await fse.mkdir(`./dist`
 		).then(
-			answer => console.log(chalk.yellowBright(`Created ./dist.`))
+			answer => console.log(chalk.yellowBright(`Created "./dist".`))
 		);
-	}
+  }
+
+	// ##### Zip the Library (child). START.
+	// package/packages/lib_structuredataghsvs/structuredataghsvs.xml
+	let zipFilename = `${name}-${version}_${versionSub}.zip`;
+	let zipFile = `${path.join(__dirname, packagesDir, zipFilename)}`;
+	let folderToZip = libDir;
+
+	let xmlFileName = `${filename}.xml`;
+	let xmlFile = `${path.join(__dirname, libDir, xmlFileName)}`;
+
+	await replaceXml.main(xmlFile);
+
+	await fse.copy(xmlFile, `./dist/${xmlFileName}`).then(
+		answer => console.log(chalk.yellowBright(
+			`Copied "${xmlFileName}" to "./dist".`))
+	);
+
+	let zip = new (require("adm-zip"))();
+	zip.addLocalFolder(folderToZip, false);
+	await zip.writeZip(zipFile);
+	console.log(chalk.cyanBright(chalk.bgRed(`"${zipFile}" written.`)));
+
+	await helper.cleanOut([libDir]);
+
+	thisPackages.push(
+		`<file type="library" id="structuredataghsvs">${zipFilename}</file>`
+	);
+	// ##### Zip the Library (child). END.
+
+	// ##### Zip the Package (main). START.
+	// package/pkg_structuredataghsvs.xml
+	zipFilename = `pkg_${zipFilename}`;
+	zipFile = `${path.join(__dirname, `dist`, zipFilename)}`;
+	folderToZip = `./package`;
+
+	xmlFileName = manifestFileName;
+	xmlFile = Manifest;
+
+	await replaceXml.main(xmlFile, null, null, thisPackages);
+
+	await fse.copy(xmlFile, `./dist/${xmlFileName}`).then(
+		answer => console.log(chalk.yellowBright(
+			`Copied "${xmlFileName}" to "./dist".`))
+	);
 
 	zip = new (require("adm-zip"))();
-	const pkgZipFilename = `pkg_${name}-${version}_${sourceInfos}.zip`;
-	zipFilename = `./dist/${pkgZipFilename}`;
-	zip.addLocalFolder('./package', false);
-	zip.writeZip(`${zipFilename}`);
-	console.log(chalk.greenBright(`./dist/${pkgZipFilename} written.`));
+	zip.addLocalFolder(folderToZip, false);
+	await zip.writeZip(zipFile);
+	console.log(chalk.cyanBright(chalk.bgRed(`"${zipFile}" written.`)));
+	// ##### Zip the Package (main). END.
+
+	const Digest = 'sha256'; //sha384, sha512
+	const checksum = await helper.getChecksum(zipFile, Digest)
+  .then(
+		hash => {
+			const tag = `<${Digest}>${hash}</${Digest}>`;
+			console.log(chalk.greenBright(`Checksum tag is: ${tag}`));
+			return tag;
+		}
+	)
+	.catch(error => {
+		console.log(error);
+		console.log(chalk.redBright(`Error while checksum creation. I won't set one!`));
+		return '';
+	});
+
+	xmlFile = 'update.xml';
+	await fse.copy(`./${xmlFile}`, `./dist/${xmlFile}`).then(
+		answer => console.log(chalk.yellowBright(
+			`Copied "${xmlFile}" to ./dist.`))
+	);
+	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename, checksum);
+
+	xmlFile = 'changelog.xml';
+	await fse.copy(`./${xmlFile}`, `./dist/${xmlFile}`).then(
+		answer => console.log(chalk.yellowBright(
+			`Copied "${xmlFile}" to ./dist.`))
+	);
+	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename, checksum);
+
+	xmlFile = 'release.txt';
+	await fse.copy(`./${xmlFile}`, `./dist/${xmlFile}`).then(
+		answer => console.log(chalk.yellowBright(
+			`Copied "${xmlFile}" to ./dist.`))
+	);
+	await replaceXml.main(`${__dirname}/dist/${xmlFile}`, zipFilename, checksum,
+		thisPackages);
 
 	cleanOuts = [
 		`./package`,
 	];
 
-	await cleanOut(cleanOuts).then(
-		answer => console.log(chalk.yellowBright(`Finish.`))
+	await helper.cleanOut(cleanOuts).then(
+		answer => console.log(chalk.cyanBright(chalk.bgRed(
+			`Finished. Good bye!`)))
 	);
 })();
